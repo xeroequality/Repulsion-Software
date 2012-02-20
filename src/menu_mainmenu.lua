ab977cf4ee06b0b513d9454c38d8e99021d663b3
Phillip Lee Fatt <pjr1991@gmail.com>
2012-02-20 14:39:16 -0500

    modified:   src/menu_mainmenu.lua 	images/background_Helpsign.png 	images/background_MPsign.png 	images/background_SPsign.png 	images/background_Settingssign.png 	images/background_earth.png

--- src/menu_mainmenu.lua
+++ src/menu_mainmenu.lua
@@ -7,6 +7,7 @@
 --
 -- Copyright 2012 Jason Simmons. All Rights Reserved.
 -- 
+
 local storyboard = require( "storyboard" )
 local scene = storyboard.newScene()
  
@@ -74,6 +75,133 @@ function scene:enterScene( event )
 		local earth = display.newImage("../images/background_earth.png")
 		earth.x = display.contentWidth/2
 		earth.y = display.contentHeight/2
+		
+		SPangle = 0+270;
+		MPangle = 90+270;
+		setangle = 180+270;
+		helpangle = 270+270;
+		increment = 0.1;
+		r = 115;
+		
+		MPsign:scale(0.5,0.5)
+		settings_Sign:scale(0.5,0.5)
+		help_Sign:scale(0.5,0.5)
+		
+		--[[
+		function rotateStuff(event)
+			earth:rotate(increment)
+			
+			SPangle = SPangle + increment;
+			MPangle = MPangle + increment;
+			setangle = setangle + increment;
+			helpangle = helpangle + increment;
+			if SPangle > 360 then SPangle = SPangle - 360 end
+			if MPangle > 360 then MPangle = MPangle - 360 end
+			if setangle > 360 then setangle = setangle - 360 end
+			if helpangle > 360 then helpangle = helpangle - 360 end
+			
+			SPsign:rotate(increment);
+			MPsign:rotate(increment);
+			settings_Sign:rotate(increment);
+			help_Sign:rotate(increment);
+			
+			-- Adjust x and y
+			SPr = SPangle*(math.pi/180);
+			SPsign.x = (r*math.cos(SPr))+(cW); SPsign.y = (r*math.sin(SPr))+cH;
+			MPr = MPangle*(math.pi/180);
+			MPsign.x = (r*math.cos(MPr))+(cW); MPsign.y = (r*math.sin(MPr))+cH;
+			setr = setangle*(math.pi/180);
+			settings_Sign.x = (r*math.cos(setr))+(cW); settings_Sign.y = (r*math.sin(setr))+cH;
+			helpr = helpangle*(math.pi/180);
+			help_Sign.x = (r*math.cos(helpr))+(cW); help_Sign.y = (r*math.sin(helpr))+cH;
+		end
+		
+		Runtime:addEventListener("enterFrame",rotateStuff)
+		--]]
+		
+		turning = false;
+		
+		function rotateStuff(event)
+		
+			if event.phase == "ended" and turning == false then
+				distance = math.sqrt( (event.x-event.xStart)^2 + (event.y-event.yStart)^2 )
+				if distance >= 25 then
+				
+					-- Rotate Clockwise
+					if event.x > event.xStart then
+						increment = 90
+						turning = true;
+						
+					-- Rotate Counterclockwise
+					else
+						increment = -90
+						turning = true;
+						
+					end
+					
+					if SPangle == 270 then SPsign:scale(0.5,0.5) end
+					if MPangle == 270 then MPsign:scale(0.5,0.5) end
+					if setangle == 270 then settings_Sign:scale(0.5,0.5) end
+					if helpangle == 270 then help_Sign:scale(0.5,0.5) end
+						
+						
+				end
+			end
+			
+		end
+		
+		function rotateAnimation(event)
+		
+			if turning == true then
+			
+				inc = 10;
+				if increment > 0 then
+					increment = increment - inc;
+				else
+					increment = increment + inc;
+					inc = inc * -1;
+				end
+		
+				SPsign:rotate(inc)
+				MPsign:rotate(inc)
+				settings_Sign:rotate(inc)
+				help_Sign:rotate(inc)
+				earth:rotate(inc)
+				
+				SPangle = SPangle + inc;
+				MPangle = MPangle + inc;
+				setangle = setangle + inc;
+				helpangle = helpangle + inc;
+				if SPangle >= 360 then SPangle = SPangle - 360 end
+				if MPangle >= 360 then MPangle = MPangle - 360 end
+				if setangle >= 360 then setangle = setangle - 360 end
+				if helpangle >= 360 then helpangle = helpangle - 360 end
+				
+				-- Adjust x and y
+				SPr = SPangle*(math.pi/180);
+				SPsign.x = (r*math.cos(SPr))+(cW); SPsign.y = (r*math.sin(SPr))+cH;
+				MPr = MPangle*(math.pi/180);
+				MPsign.x = (r*math.cos(MPr))+(cW); MPsign.y = (r*math.sin(MPr))+cH;
+				setr = setangle*(math.pi/180);
+				settings_Sign.x = (r*math.cos(setr))+(cW); settings_Sign.y = (r*math.sin(setr))+cH;
+				helpr = helpangle*(math.pi/180);
+				help_Sign.x = (r*math.cos(helpr))+(cW); help_Sign.y = (r*math.sin(helpr))+cH;
+				
+				if increment == 0 then
+					turning = false
+					
+					if SPangle == 270 then SPsign:scale(2,2) end
+					if MPangle == 270 then MPsign:scale(2,2) end
+					if setangle == 270 then settings_Sign:scale(2,2) end
+					if helpangle == 270 then help_Sign:scale(2,2) end
+				end
+
+			end
+		
+		end
+		
+		sky:addEventListener("touch",rotateStuff);
+		Runtime:addEventListener("enterFrame",rotateAnimation);
 		
         
 end

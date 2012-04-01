ab306b28e43240c027e47f2487028a1e678e9e6d
Phillip Lee Fatt <pjr1991@gmail.com>
2012-04-01 12:49:42 -0400

    modified:   src/main.lua 	new file:   src/slot1.lua 	new file:   src/slot2.lua

---
+++ src/slot2.lua
@@ -0,0 +1,30 @@
+local Material = require("materials")
+
+PlayerBase = {}
+
+PlayerBase.structure = {
+numObjects=4,
+baseX=206.90734863281,
+baseY=306.5869140625,
+totalCost=200,
+id={
+4,4,4,4
+},
+x_vals={
+5.0084838867188,12.121688842773,0,6.042724609375
+},
+y_vals={
+0,-6.3948059082031,-12.795196533203,-19.224731445313
+},
+scaleX={
+1,1,1,1
+},
+scaleY={
+1,1,1,1
+},
+rotations={
+0.010299747809768,-0.028589947149158,-0.069960094988346,-0.050005152821541
+}
+}
+
+return PlayerBase

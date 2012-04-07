0421194bc88de6611292c971164f8c3f222ea71a
Phillip Lee Fatt <pjr1991@gmail.com>
2012-04-07 10:19:03 -0400

    modified:   src/main.lua 	new file:   src/slot8.lua

---
+++ src/slot8.lua
@@ -0,0 +1,30 @@
+local Material = require("materials")
+
+PlayerBase = {}
+
+PlayerBase.structure = {
+numObjects=2,
+baseX=110.45345306396,
+baseY=291.58276367188,
+totalCost=550,
+id={
+12,1000
+},
+x_vals={
+23.235847473145,0
+},
+y_vals={
+0,-41.3193359375
+},
+scaleX={
+0.16666667163372,0.33333334326744
+},
+scaleY={
+0.16666667163372,0.33333334326744
+},
+rotations={
+-0.064823396503925,-0.26916825771332
+}
+}
+
+return PlayerBase

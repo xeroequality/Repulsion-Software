4187c11eb897fe7ba9887bcb1f061aa63dbf6f7b
David Greene <Djgreene730@Gmail.com>
2012-04-09 15:02:38 -0400

    modified:   src/units.lua

--- src/units.lua
+++ src/units.lua
@@ -219,7 +219,7 @@ Unit.cannon = {
 
 			else
 				-- Move View Back to User's Base				
-				for i = Parallax.currentView.x, 0, -0.1 do
+				for i = Parallax.currentView.x, 0, -1 do
 					print(Parallax.currentView.x);
 					Parallax.move_abs(Parallax.currentView.x + ((i - Parallax.currentView.x) * 0.01), 0, "moved");
 				end

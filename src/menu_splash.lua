c7ac0e1668ff7c55da4bae2ee3f274cd229fec47
Nick <nickolas1230@hotmail.com>
2012-04-28 16:13:25 -0400

    Changed intro music
    Signed-off-by: Nick <nickolas1230@hotmail.com>

--- src/menu_splash.lua
+++ src/menu_splash.lua
@@ -63,7 +63,7 @@ function scene:enterScene( event )
 		local w = display.contentWidth
 		local h = display.contentHeight
 		
-		local o_fortuna = audio.loadStream("../sound/alienIntroMusic.mp3")
+		local o_fortuna = audio.loadStream("../sound/Drain The Lizard 30.mp3")
 		local o_play = audio.play(o_fortuna, {loops=-1, fadein=0})
 
 		--Make the Space Backgrounds
